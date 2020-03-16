899c09d42c62eeee796c5eef91c91c855276d552
r1o.makaroff <r1o.makaroff@yandex.ru>
2020-03-17 02:42:24 +0300

    looking for a bug among several pages: added several links with the offerN parameter to search for a bug for test_product_page.py

--- pages/product_page.py
+++ pages/product_page.py
@@ -14,13 +14,13 @@ class ProductPage(BasePage):
     def check_name_product(self):
         product_in_basket_name = self.browser.find_element(*ProductPageLocators.PRODUCT_IN_BASKET_NAME)
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
-        print(f'\n{product_in_basket_name.text}\n{product_name.text}')
-        assert product_name.text == product_name.text, "Product names do not match:("
+        #print(f'\n{product_in_basket_name.text}\n{product_name.text}')
+        assert product_in_basket_name.text == product_name.text, "Product names do not match:("
         time.sleep(1)
 
     def check_price_product(self):
         product_in_basket_price = self.browser.find_element(*ProductPageLocators.PRODUCT_IN_BASKET_PRICE)
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
-        print(f'\n{product_in_basket_price.text}\n{product_price.text}')
-        assert product_price.text == product_price.text, "Product price do not match:("
+       # print(f'\n{product_in_basket_price.text}\n{product_price.text}')
+        assert product_in_basket_price.text == product_price.text, "Product price do not match:("
         time.sleep(1)
--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,29 @@
 from .pages.product_page import ProductPage
+import pytest
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+# link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 
 
-def test_guest_can_add_product_to_basket(browser):
+@pytest.mark.parametrize('links', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, links):
+    link = f"{links}"
+    print(link)
     page = ProductPage(browser, link)
     page.open()
     page.click_add_to_basket_form()
     page.check_name_product()
     page.check_price_product()
+
+    # @pytest.mark.parametrize('link', ["okay_link",
+    #                                   pytest.param("bugged_link", marks=pytest.mark.xfail),
+    #                                   "okay_link"])
