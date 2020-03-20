dc02c56474333b12ab6ced576ba82436e6ca4b68
r1o.makaroff <r1o.makaroff@yandex.ru>
2020-03-21 00:33:28 +0300

    finish add test marker

--- test_product_page.py
+++ test_product_page.py
@@ -11,7 +11,8 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?prom
 #link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
 
 
-@pytest.mark.skip
+#@pytest.mark.skip
+@pytest.mark.need_review
 @pytest.mark.parametrize('links', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -19,7 +20,8 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?prom
                                    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
                                    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
                                    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                                   marks=pytest.mark.xfail(reason="bug")),
                                    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 def test_guest_can_add_product_to_basket(browser, links):
@@ -74,13 +76,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
-@pytest.mark.skip
+#@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
-@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     # Гость открывает главную страницу
     page = ProductPage(browser, link)
@@ -118,6 +121,7 @@ class TestUserAddToBasketFromProductPage():
         # Проверяем, что нет сообщения об успехе с помощью is_not_element_present
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     # @pytest.mark.skip
     def test_user_can_add_product_to_basket(self):
         print(link)
